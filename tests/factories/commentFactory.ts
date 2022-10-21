1ab882127961f8c3074357471e9564ecba646e04
Matheus-daSilva <matheus2001silva@live.com>
2022-10-20 21:49:23 -0300

    remove: tests files

--- tests/factories/commentFactory.ts
+++
@@ -1,7 +0,0 @@
-import prisma from "../../src/config/database.js"
-
-export async function findComment(publicationId: number) {
-    return await prisma.comment.findFirst({
-        where: {publicationId}
-    })
-}
--- tests/factories/userFactory.ts
+++
@@ -1,8 +0,0 @@
-import { UserData } from "../../src/repositories/authRepository.js"
-import { authService } from "../../src/service/authService.js"
-
-export async function createUserFactory(user: UserData) {
-    const respo = await authService.signUp(user)
-    const respo2 = await authService.signIn({email: user.email, password: user.password})
-    return respo2
-}
